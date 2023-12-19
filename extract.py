fdc99a939f73908845123cd40f2b02ce1f990e3c
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2023-12-19 10:50:16 +0100

    make merging of headers optional

--- extract.py
+++ extract.py
@@ -2,7 +2,7 @@ import json
 import sys
 from tqdm import tqdm
 
-from config import DEBUG
+from config import DEBUG, MERGE
 
 def merge(h1, h2, m):
     if not h1:
@@ -44,21 +44,29 @@ if __name__ == "__main__":
         if DEBUG:
             print(f"merged_header = {merged_header}")
         assert(len(merged_header) == len(set(merged_header)))
+        assert(not any("\t" in h for h in merged_header))
         new_rows = []
-        for variant in variants:
+        for i, variant in enumerate(variants):
+            if not MERGE:
+                new_rows.clear()
             header = variant["header"]
             fine_rows = list(variant["fine"])
             for row in tqdm(fine_rows, desc=f"Extracting rows for {arg}"):
                 assert(len(header) == len(row))
                 row2val = dict(zip(header, row))
-                new_row = [row2val.get(h, '') for h in merged_header]
+                new_row = [row2val.get(h, '') for h in (merged_header if MERGE else header)]
                 if DEBUG:
                     print(f"new_row = {new_row}")
                 new_rows.append(new_row)
-        assert(not any("\t" in new_row for new_row in new_rows))
-        assert(not any("\t" in h for h in merged_header))
-        with open(arg.replace("data_fixed/", "data_extracted/").replace(".json", ".tsv"), "wt") as f:
-            print("\t".join(merged_header), file=f)
-            for new_row in new_rows:
-                print("\t".join(new_row), file=f)
+            assert(not any("\t" in new_row for new_row in new_rows))
+            if not MERGE:
+                with open(arg.replace("data_fixed/", "data_extracted/").replace(".json", f"-header-{i}.tsv"), "wt") as f:
+                    print("\t".join(header), file=f)
+                    for new_row in new_rows:
+                        print("\t".join(new_row), file=f)
+        if MERGE:
+            with open(arg.replace("data_fixed/", "data_extracted/").replace(".json", ".tsv"), "wt") as f:
+                print("\t".join(merged_header), file=f)
+                for new_row in new_rows:
+                    print("\t".join(new_row), file=f)
 

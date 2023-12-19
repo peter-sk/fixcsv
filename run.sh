74b7ed7775f5e58f240c7d549087bbee429684f9
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2023-12-19 10:57:30 +0100

    extract standardized values instead of original values create expected folders in script

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -v data_splitted data_cleaned data_augmented data_fixed data_extracted
 python split.py $(find data/* -type f) && \
 python clean.py data_splitted/* && \
 python augment.py data_cleaned/* && \
